c833fdbc6d4c16cc0a9f3f7926df4733ebc6d061
durotolu <durolawk@gmail.com>
2024-06-24 21:08:54 +0100

    built actions modal

---
+++ lendsqr-app/src/components/ActionsModal/ActionsModal.tsx
@@ -0,0 +1,27 @@
+import "./ActionsModal.scss";
+import view from "../../icons/view.svg";
+import activate from "../../icons/activate.svg";
+import blacklist from "../../icons/blacklist.svg";
+
+function Actions() {
+  return (
+    <div className="popup">
+      <ul>
+        <li>
+          <img src={view} alt="view" />
+          View Details
+        </li>
+        <li>
+          <img src={blacklist} alt="blacklist" />
+          Blacklist User
+        </li>
+        <li>
+          <img src={activate} alt="activate" />
+          Activate User
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Actions;
