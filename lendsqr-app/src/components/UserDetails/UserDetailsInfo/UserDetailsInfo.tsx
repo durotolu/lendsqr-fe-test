ed0ed8a01c54e556b6309ce51a92a4cd276d8769
durotolu <durolawk@gmail.com>
2024-06-25 20:49:40 +0100

    updated user details

--- lendsqr-app/src/components/UserDetails/UserDetailsInfo/UserDetailsInfo.tsx
+++ lendsqr-app/src/components/UserDetails/UserDetailsInfo/UserDetailsInfo.tsx
@@ -2,82 +2,87 @@ import UserItem from "../../../types";
 import "./UserDetailsInfo.scss";
 
 function UserDetailsInfo({ user }: { user: UserItem }) {
+  console.log(user);
+  const title = {
+    fullName: "FULL NAME",
+    phoneNumber: "PHONE NUMBER",
+    email: "EMAIL ADDRESS",
+    emailAddress: "EMAIL ADDRESS",
+    maritalStatus: "MARITAL STATUS",
+    childCount: "CHILDREN",
+    residenceType: "Type of residence",
+    employmentDuration: "Duration of employmen",
+    employmentStatus: "employment status",
+    employmentSector: "sector of employment",
+    officeEmail: "office email",
+    monthlyIncome: "Monthly income",
+    loanRepayment: "loan repayment",
+    educationLevel: "level of education",
+  };
   return (
     <div className="details-body">
       <div className="row">
         <h3>Personal Information</h3>
         <div className="row-body-container">
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
+          {Object.entries(user.personalInformation).map(([key, value]) => (
+            <div key={key} className="row-body">
+              <div>
+                <div className="title">
+                  {title[key as keyof typeof title] || key}
+                </div>
+                <div className="content">{value}</div>
+              </div>
             </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="row">
-        <h3>Personal Information</h3>
+        <h3>Education and Employment</h3>
         <div className="row-body-container">
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
+          {Object.entries(user.educationAndEmployment).map(([key, value]) => (
+            <div key={key} className="row-body">
+              <div>
+                <div className="title">
+                  {title[key as keyof typeof title] || key}
+                </div>
+                <div className="content">{value}</div>
+              </div>
             </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
+          ))}
+        </div>
+      </div>
+      <div className="row">
+        <h3>Socials</h3>
+        <div className="row-body-container">
+          {Object.entries(user.socials).map(([key, value]) => (
+            <div key={key} className="row-body">
+              <div>
+                <div className="title">
+                  {title[key as keyof typeof title] || key}
+                </div>
+                <div className="content">{value}</div>
+              </div>
             </div>
-          </div>
-          <div className="row-body">
-            <div>
-              <div className="title">full Name</div>
-              <div className="content">Grace Effiom</div>
+          ))}
+        </div>
+      </div>
+      <div className="row">
+        <h3>Guarantor</h3>
+        <div className="row-guarantor">
+          {user.guarantor.map((person) => (
+            <div className="row-body-container guarantor">
+              {Object.entries(person).map(([key, value]) => (
+                <div key={key} className="row-body">
+                  <div>
+                    <div className="title">
+                      {title[key as keyof typeof title] || key}
+                    </div>
+                    <div className="content">{value}</div>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
