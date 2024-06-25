deef1e493795f0d9504afc90be35d0db9b69a5f1
durotolu <durolawk@gmail.com>
2024-06-25 11:42:49 +0100

    added types

---
+++ lendsqr-app/src/types.ts
@@ -0,0 +1,55 @@
+interface MetaData {
+  code: string;
+  tier: string;
+  accountNumber: string;
+  accountBalance: string;
+  bank: string;
+  dateJoined: string;
+  status: string;
+  loans: boolean;
+  savings: boolean;
+  organization: string;
+}
+
+interface PersonalInformation {
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  bvn: string;
+  gender: string;
+  maritalStatus: string;
+  childCount: number;
+  residenceType: string;
+}
+
+interface EducationAndEmployment {
+  educationLevel: string;
+  employmentStatus: string;
+  employmentSector: string;
+  employmentDuration: string;
+  officeEmail: string;
+  monthlyIncome: string;
+  loanRepayment: string;
+}
+
+interface Socials {
+  twitter: string;
+  facebook: string;
+  instagram: string;
+}
+
+interface Guarantor {
+  fullName: string;
+  phoneNumber: string;
+  emailAddress: string;
+  relationship: string;
+}
+
+export default interface UserItem {
+  id: string;
+  metaData: MetaData;
+  personalInformation: PersonalInformation;
+  educationAndEmployment: EducationAndEmployment;
+  socials: Socials;
+  guarantor: Guarantor[];
+}
