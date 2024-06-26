bb2612322c6a0da344b2ca6154cc3da384ef3e41
durotolu <durolawk@gmail.com>
2024-06-26 22:11:14 +0100

    added logout

--- lendsqr-app/src/components/Header/Header.tsx
+++ lendsqr-app/src/components/Header/Header.tsx
@@ -4,8 +4,23 @@ import search_icon from "../../icons/search-icon.svg";
 import notification from "../../icons/notification.svg";
 import avatar from "../../icons/avatar.svg";
 import dropdown from "../../icons/dropdown.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    localStorage.removeItem("lendsqrUserEmail");
+
+    await localStorage.removeItem("lendsqrUserEmail");
+    const email = await localStorage.getItem("lendsqrUserEmail");
+    if (!email) {
+      navigate("/");
+    }
+  };
+
   return (
     <header>
       <div className="search-nav">
@@ -19,7 +34,7 @@ function Header() {
           </div>
         </div>
       </div>
-      <div>
+      <div className="profile-right">
         <div className="profile-nav">
           <div>
             <a href="/#">Docs</a>
@@ -27,13 +42,24 @@ function Header() {
           <div>
             <img src={notification} alt="notification bell" />
           </div>
-          <div>
+          <div className="dropdown" onClick={() => setIsOpen(!isOpen)}>
             <img src={avatar} alt="avatar" />
             <span className="profile-name">Adedeji</span>
             <img src={dropdown} alt="dropdown" />
+            {isOpen && (
+              <div className="popup-profile" onClick={logout}>
+                <div>Logout</div>
+              </div>
+            )}
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="modal-backdrop"
+          onClick={() => setIsOpen(!isOpen)}
+        ></div>
+      )}
     </header>
   );
 }
